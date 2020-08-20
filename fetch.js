f0d67e79b1911ee6bb71b9af0e7aca87f504c9c8
Anna Udziela <annaudziela@gmail.com>
2020-08-20 15:26:35 +0200

    final commit

--- fetch.js
+++ fetch.js
@@ -7,7 +7,7 @@ fetch('https://api.github.com/users/annaudziela/repos?sort=created&direction=asc
     const {name, html_url} = repo;
     const repositoryList = document.querySelector('.list__details--js');
     const myTemplate = `<li>
-    ${name} <a href="${html_url}" title="link to ${name} repository on gitHub"> go to Github</a>
+    ${name} || <a href="${html_url}" title="link to ${name} repository on gitHub"> go to Github</a>
     </li>`;
     repositoryList.innerHTML += myTemplate;
 }
@@ -23,14 +23,14 @@ if (isDark) {
     document.documentElement.style.setProperty('--background-color', 'whitesmoke');
     document.documentElement.style.setProperty('--text-color', '#373F47');
     document.documentElement.style.setProperty('--border-style', '#373F47 1px solid');
+    document.documentElement.style.setProperty('--link-color', 'rgba(0, 0, 0, 0.781)');
     isDark = false; 
 } else {
 document.documentElement.style.setProperty('--background-color', 'rgba(140, 144, 144, 0.800)');
     document.documentElement.style.setProperty('--text-color', 'whitesmoke');
     document.documentElement.style.setProperty('--border-style', 'whitesmoke 1px solid');
+    document.documentElement.style.setProperty('--link-color', 'whitesmoke');
     isDark = true; 
 }
 });
 
-// --background-color: whitesmoke; 
-// --text-color: #373F47;
